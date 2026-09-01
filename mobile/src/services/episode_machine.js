d990bfa6d576ce68a4eb1b40e88b96c240477818
tatiana yammine <8g7jyj629d@privaterelay.appleid.com>
2026-09-01 23:15:51 +0300

    Demo-pace episode timing: 15s watch, 10s unanswered check-in to camera

--- mobile/src/services/episode_machine.js
+++ mobile/src/services/episode_machine.js
@@ -13,8 +13,8 @@ export const EpisodeState = {
 };
 
 export const TIMING = {
-  watchingMs: 30 * 1000,
-  checkInMs: 30 * 1000,
+  watchingMs: 15 * 1000,
+  checkInMs: 10 * 1000,
   supportMs: 150 * 1000,
   recoveryMs: 120 * 1000,
   maxInterventions: 3,
